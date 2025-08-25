c6fd78bd029d24a33501f892e2ef6cb84a586b8d
castledragon <starlight1532@gmail.com>
2025-08-25 22:30:59 +0900

    init commit / front / back

---
+++ castleboot/src/main/java/com/castle/chat/castleboot/CastlebootApplication.java
@@ -0,0 +1,13 @@
+package com.castle.chat.castleboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CastlebootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CastlebootApplication.class, args);
+	}
+
+}
---
+++ castleboot/src/test/java/com/castle/chat/castleboot/CastlebootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.castle.chat.castleboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CastlebootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
