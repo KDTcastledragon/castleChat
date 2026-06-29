490788fb4f13c8985ed28732fd59edc15efd9dae
castledragon <starlight1532@gmail.com>
2026-06-29 17:07:36 +0900

    orc send설계 마감.

--- castledragon/chat-orchestrator/src/main/java/com/chat/chatorc/mapper/OrcChatMapper.java
+++ castledragon/chat-orchestrator/src/main/java/com/chat/chatorc/mapper/OrcChatMapper.java
@@ -1,5 +1,7 @@
 package com.chat.chatorc.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import com.chat.contract.domain.ChatMessagesDTO;
@@ -7,4 +9,6 @@ import com.chat.contract.domain.ChatMessagesDTO;
 @Mapper
 public interface OrcChatMapper {
 	int insertChatMessage(ChatMessagesDTO dto);
+
+	List<Long> findAllActiveMemberIdsInRoom(Long roomId);
 }
--- castledragon/chat-orchestrator/src/main/java/com/chat/chatorc/service/ChatOrcCommandService.java
+++ castledragon/chat-orchestrator/src/main/java/com/chat/chatorc/service/ChatOrcCommandService.java
@@ -1,5 +1,6 @@
 package com.chat.chatorc.service;
 
+import java.time.LocalDateTime;
 import java.util.Set;
 
 import org.springframework.stereotype.Service;
@@ -10,6 +11,7 @@ import com.chat.chatorc.mapper.OrcChatMapper;
 import com.chat.chatorc.usecase.ChatOrcCommandUseCase;
 import com.chat.contract.command.CreateChatMessageCommand;
 import com.chat.contract.domain.ChatMessageViewDTO;
+import com.chat.contract.domain.ChatMessagesDTO;
 import com.chat.redis.cache.RoomMemberCache;
 
 import lombok.RequiredArgsConstructor;
@@ -28,6 +30,7 @@ public class ChatOrcCommandService implements ChatOrcCommandUseCase {
 	@Override
 	@Transactional
 	public ChatMessageViewDTO createChatMessage(CreateChatMessageCommand command) {
+		// 필수 인자 여부 검증.
 		if (command.getRoomId() == null) {
 			log.error("id:{} 채팅의 roomId없음 : {}", command.getSenderUserId(), command.getRoomId());
 			throw new IllegalArgumentException("No RoomId");
@@ -38,10 +41,46 @@ public class ChatOrcCommandService implements ChatOrcCommandUseCase {
 			throw new IllegalArgumentException("no msg");
 		}
 
-		Set<Long> allActiveMemberIdsInRoom = roomMemberCache.getOrLoadRoomMembers();
+		// 채팅방 내 모든 멤버 불러오기. 현재 sender가 실제 이 채팅방의 멤버인지 검증하기 위함.
+		Set<Long> allActiveMemberIdsInRoom = roomMemberCache
+				.getOrLoadRoomMembers(command.getRoomId(), () -> chatMapper.findAllActiveMemberIdsInRoom(command.getRoomId()));
 
-		int isCreated = chatMapper.insertChatMessage(null);
-		return null;
+		if (allActiveMemberIdsInRoom.isEmpty()) {
+			log.error("id:{}의 채팅방 멤버 정보를 찾을 수 없음. {}", command.getSenderUserId(), allActiveMemberIdsInRoom);
+			throw new IllegalArgumentException("채팅방 멤버 정보를 찾을 수 없음.");
+		}
+
+		if (!allActiveMemberIdsInRoom.contains(command.getSenderUserId())) {
+			log.error("id:{}는 {}-채팅방의 멤버가 아닙니다. {}", command.getSenderUserId(), command.getRoomId(), allActiveMemberIdsInRoom);
+			throw new IllegalArgumentException("채팅방 멤버가 아닙니다.");
+		}
+
+		LocalDateTime now = LocalDateTime.now();
+
+		ChatMessagesDTO msg = new ChatMessagesDTO();
+		msg.setRoomId(command.getRoomId());
+		msg.setSenderId(command.getSenderUserId());
+		msg.setMessageText(command.getMessageText());
+		msg.setCreatedAt(now);
+
+		// DB insert
+		int isCreated = chatMapper.insertChatMessage(msg); // useGK + keyP 설정 => msgId PK 사용 가능. 
+
+		if (isCreated != 1) {
+			throw new IllegalStateException("insert Msg Failed");
+		}
+
+		Long unreadCount = Math.max(allActiveMemberIdsInRoom.size() - 1L, 0L); // 메시지 읽지 않은 멤버 수. (sender 제외)
+
+		ChatMessageViewDTO response = new ChatMessageViewDTO();
+		response.setMessageId(msg.getMessageId());
+		response.setRoomId(msg.getRoomId());
+		response.setSenderPublicId(command.getSenderPublicId());
+		response.setMessageText(msg.getMessageText());
+		response.setCreatedAt(msg.getCreatedAt());
+		response.setUnreadCount(unreadCount);
+
+		return response;
 	}
 
 	@Override
